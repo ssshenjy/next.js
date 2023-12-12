16ae6ac6e4db6334468a225772aa812ecdaf8aba
janiskim0716 <92025120+janiskim0716@users.noreply.github.com>
2023-12-11 21:24:54 -0800

    Update normalize-version-bump.js
    Modularization, consistent naming, comments, sequential execution, simplified loops

--- scripts/normalize-version-bump.js
+++ scripts/normalize-version-bump.js
@@ -1,76 +1,75 @@
 #!/usr/bin/env node
 // @ts-check
 
-/*
- This prevents busting the turbo cache un-necessarily due 
- to bumping the version in the repo's package.json files
-*/
-const path = require('path')
-const fs = require('fs/promises')
+const path = require('path');
+const fs = require('fs/promises');
 
-const cwd = process.cwd()
-const NORMALIZED_VERSION = '0.0.0'
+const cwd = process.cwd();
+const NORMALIZED_VERSION = '0.0.0';
 
-const readJson = async (filePath) =>
-  JSON.parse(await fs.readFile(filePath, 'utf8'))
+// Utility function to read JSON from a file
+const readJson = async (filePath) => JSON.parse(await fs.readFile(filePath, 'utf8'));
 
+// Utility function to write JSON to a file
 const writeJson = async (filePath, data) =>
-  fs.writeFile(filePath, JSON.stringify(data, null, 2) + '\n')
+  fs.writeFile(filePath, JSON.stringify(data, null, 2) + '\n');
 
-;(async function () {
-  const packages = await fs.readdir(path.join(cwd, 'packages'))
+// Function to normalize versions in a given package
+const normalizePackageVersions = async (pkgDir, pkgNames) => {
+  const filePath = path.join(cwd, 'packages', pkgDir, 'package.json');
+  const data = await readJson(filePath);
+  const version = data.version;
 
-  const pkgJsonData = new Map()
-  const pkgNames = []
-  await Promise.all(
-    packages.map(async (pkgDir) => {
-      const data = await readJson(
-        path.join(cwd, 'packages', pkgDir, 'package.json')
-      )
-      pkgNames.push(data.name)
-      pkgJsonData.set(pkgDir, data)
-    })
-  )
-  const normalizeVersions = async (filePath, data) => {
-    data = data || (await readJson(filePath))
-    const version = data.version
+  if (version) {
+    data.version = NORMALIZED_VERSION;
 
-    if (version) {
-      data.version = NORMALIZED_VERSION
-      const normalizeEntry = (type, key) => {
-        const pkgVersion = data[type][key]
+    const normalizeEntry = (type, key) => {
+      const pkgVersion = data[type][key];
 
-        if (pkgNames.includes(key) && pkgVersion === version) {
-          data[type][key] = NORMALIZED_VERSION
-        }
+      if (pkgNames.includes(key) && pkgVersion === version) {
+        data[type][key] = NORMALIZED_VERSION;
       }
-      for (const key of Object.keys(data.dependencies || {})) {
-        normalizeEntry('dependencies', key)
-      }
-      for (const key of Object.keys(data.devDependencies || {})) {
-        normalizeEntry('devDependencies', key)
-      }
-      await writeJson(filePath, data)
-    }
+    };
+
+    Object.keys(data.dependencies || {}).forEach((key) => normalizeEntry('dependencies', key));
+    Object.keys(data.devDependencies || {}).forEach((key) => normalizeEntry('devDependencies', key));
+
+    await writeJson(filePath, data);
   }
-  await Promise.all(
-    packages.map((pkgDir) =>
-      normalizeVersions(
-        path.join(cwd, 'packages', pkgDir, 'package.json'),
-        pkgJsonData.get(pkgDir)
-      )
-    )
-  )
-  await normalizeVersions(path.join(cwd, 'lerna.json'))
-  await fs.unlink(path.join(cwd, 'pnpm-lock.yaml'))
-  await fs.writeFile(path.join(cwd, 'pnpm-lock.yaml'), '')
+};
 
-  const rootPkgJsonPath = path.join(cwd, 'package.json')
+// Main function to normalize versions in all packages
+const normalizeVersionsInPackages = async () => {
+  const packages = await fs.readdir(path.join(cwd, 'packages'));
+  const pkgNames = [];
+
+  const pkgJsonData = await Promise.all(
+    packages.map(async (pkgDir) => {
+      const data = await readJson(path.join(cwd, 'packages', pkgDir, 'package.json'));
+      pkgNames.push(data.name);
+      return { pkgDir, data };
+    })
+  );
+
+  await Promise.all(pkgJsonData.map(({ pkgDir }) => normalizePackageVersions(pkgDir, pkgNames)));
+};
+
+// Main function to run the version normalization script
+const runVersionNormalizationScript = async () => {
+  await normalizeVersionsInPackages();
+  await normalizePackageVersions('', []); // Normalize lerna.json
+  await fs.unlink(path.join(cwd, 'pnpm-lock.yaml'));
+  await fs.writeFile(path.join(cwd, 'pnpm-lock.yaml'), '');
+
+  const rootPkgJsonPath = path.join(cwd, 'package.json');
   await writeJson(rootPkgJsonPath, {
     name: 'nextjs-project',
     version: '0.0.0',
     private: true,
     workspaces: ['packages/*'],
     scripts: {},
-  })
-})()
+  });
+};
+
+// Execute the main script
+runVersionNormalizationScript();
