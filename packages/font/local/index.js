83971611e99b5b92fc1351965e995e66c1b59579
janiskim0716 <92025120+janiskim0716@users.noreply.github.com>
2023-12-11 20:44:58 -0800

    Update index.js
    Added consistent commenting, removed redundant backpacks in the error message, and added semicolons at the end of statements for consistency.

--- packages/font/local/index.js
+++ packages/font/local/index.js
@@ -1,15 +1,21 @@
+const semver = require('next/dist/compiled/semver');
+const nextPackage = require('next/package.json');
+
 // Validate next version
-const semver = require('next/dist/compiled/semver')
-if (semver.lt(require('next/package.json').version, '13.0.0')) {
-  throw new Error('`@next/font` is only available in Next.js 13 and newer.')
+if (semver.lt(nextPackage.version, '13.0.0')) {
+  throw new Error('`@next/font` is only available in Next.js 13 and newer.');
 }
 
-let message = '@next/font/local failed to run or is incorrectly configured.'
+// Prepare error message
+let message = '@next/font/local failed to run or is incorrectly configured.';
+
+// Provide additional information in development mode
 if (process.env.NODE_ENV === 'development') {
-  message +=
-    '\nIf you just installed `@next/font`, please try restarting `next dev` and resaving your file.'
+  message += `\nIf you just installed \`@next/font\`, please try restarting \`next dev\` and resaving your file.`;
 }
 
-message += `\n\nRead more: https://nextjs.org/docs/basic-features/font-optimization`
+// Add a link to documentation
+message += '\n\nRead more: https://nextjs.org/docs/basic-features/font-optimization';
 
-throw new Error(message)
+// Throw an error with the prepared message
+throw new Error(message);
