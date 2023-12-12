b68464a4c8029bc68b57703909450df86e5c4323
janiskim0716 <92025120+janiskim0716@users.noreply.github.com>
2023-12-11 20:38:41 -0800

    Update index.js
    Added comments to explain the purpose of the code blocks especially because this code is part of a larger codebase and may be maintained by others.

--- packages/font/google/index.js
+++ packages/font/google/index.js
@@ -1,15 +1,21 @@
 // Validate next version
 const semver = require('next/dist/compiled/semver')
+
+// Check if Next.js version is 13.0.0 or newer
 if (semver.lt(require('next/package.json').version, '13.0.0')) {
   throw new Error('`@next/font` is only available in Next.js 13 and newer.')
 }
 
+// Prepare error message
 let message = '@next/font/google failed to run or is incorrectly configured.'
+
+// Provide additional information in development mode
 if (process.env.NODE_ENV === 'development') {
-  message +=
-    '\nIf you just installed `@next/font`, please try restarting `next dev` and resaving your file.'
+  message += `\nIf you just installed \`@next/font\`, please try restarting \`next dev\` and resaving your file.`
 }
 
-message += `\n\nRead more: https://nextjs.org/docs/basic-features/font-optimization`
+// Add a link to documentation
+message += '\n\nRead more: https://nextjs.org/docs/basic-features/font-optimization'
 
+// Throw an error with the prepared message
 throw new Error(message)
